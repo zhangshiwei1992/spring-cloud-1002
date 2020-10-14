2518f8df65bd233615a40b6a84b624bde1edfbc8
zhangshiwei <zhangshiwei199204@163.com>
2020-10-14 13:07:35 +0800

    熔断降级与请求超时降级 - Hystrix

--- spring-cloud-order-service-8082/order-service-provider/src/main/java/com/zsw/orderserviceprovider/controller/OrderController.java
+++ spring-cloud-order-service-8082/order-service-provider/src/main/java/com/zsw/orderserviceprovider/controller/OrderController.java
@@ -16,9 +16,21 @@ import org.springframework.web.bind.annotation.RestController;
 public class OrderController {
     public final static SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
-    @RequestMapping("/orders")
-    public String getEnv() {
-        System.out.println("OrderController orders : " + simpleDateFormat.format(new Date()));
+    @RequestMapping("/getOrder")
+    public String getOrder() {
+        System.out.println("OrderController getOrder , 请求时间: " + simpleDateFormat.format(new Date()));
+        return "orders-100个";
+    }
+
+    @RequestMapping("/getOrderTimeOut")
+    public String getOrderTimeOut() {
+        System.out.println("OrderController getOrderTimeOut , 请求时间: " + simpleDateFormat.format(new Date()));
+        try {
+            System.out.println("OrderController getOrderTimeOut 开始沉睡5秒 , 请求时间: " + simpleDateFormat.format(new Date()));
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         return "orders-100个";
     }
 
--- spring-cloud-user-service-8081/user-service-provider/src/main/java/com/zsw/userserviceprovider/hystrix/TestHystrixController.java
+++ spring-cloud-user-service-8081/user-service-provider/src/main/java/com/zsw/userserviceprovider/hystrix/TestHystrixController.java
@@ -26,10 +26,17 @@ public class TestHystrixController {
     @Autowired
     private RestTemplate                 restTemplate;
 
-    @HystrixCommand(commandProperties = { @HystrixProperty(name = "circuitBreaker.enabled", value = "true"),
+    /**
+     * 熔断触发降级 - 请求失败
+     *
+     * @param num 个数
+     * @return 结果
+     */
+    @HystrixCommand(fallbackMethod = "zswHystrixFallback", commandProperties = {
+            @HystrixProperty(name = "circuitBreaker.enabled", value = "true"),
             @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "5"),
             @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value = "5000"),
-            @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "50") }, fallbackMethod = "zswUserFallback") // 请求失败触发的方法
+            @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "50") })
     @RequestMapping("/order/{num}")
     public String queryOrder(@PathVariable Integer num) {
         System.out.println("TestHystrixController queryOrder ------ num: " + num + ",  时间: "
@@ -41,20 +48,55 @@ public class TestHystrixController {
         }
 
         System.out.println("num是奇数 - 访问远程 - 订单信息服务器8082 - 如果调不通就走断路器");
-        return restTemplate.getForObject("http://127.0.0.1:8082/orders", String.class);
+        return restTemplate.getForObject("http://127.0.0.1:8082/getOrder", String.class);
     }
 
     /**
-     * 回退方案(降级处理方案)
+     * 熔断触发降级 - 回退方案(降级处理方案)
      *
      * @param num 请求参数
      * @return 结果
      */
-    public String zswUserFallback(Integer num) {
+    public String zswHystrixFallback(Integer num) {
         // 可以在此做兜底的数据处理,例如从广告服务器上查询最新的广告失败了,可以返回固定的广告信息
-        System.out.println("TestHystrixController zswUserFallback ------ num: " + num + "  时间: "
+        System.out.println("TestHystrixController zswHystrixFallback ------ num: " + num + "  时间: "
                 + simpleDateFormat.format(new Date()));
-        return "zswUserFallback - 回退方案 - 请求失败!!!";
+        return "熔断触发降级 - zswHystrixFallback - 回退方案 - 请求失败!!!";
+    }
+
+    /**
+     * 请求超时 - 触发降级 (默认的timeoutInMilliseconds , 是1000 即 1s, 1秒钟远程连接还未响应就会触发熔断)
+     *
+     * @param num 个数
+     * @return 结果
+     */
+    @HystrixCommand(fallbackMethod = "zswTimeoutFallback", commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "3000"), })
+    @RequestMapping("/queryOrderTimeout/{num}")
+    public String queryOrderTimeout(@PathVariable Integer num) {
+        System.out.println("TestHystrixController queryOrderTimeout ------ num: " + num + ",  时间: "
+                + simpleDateFormat.format(new Date()));
+
+        if (num % 2 == 0) {
+            System.out.println("num是偶数 - 不走远程访问 - 不会触发断路器逻辑");
+            return "num是偶数 - 不走远程访问 - 不会触发断路器逻辑";
+        }
+
+        System.out.println("num是奇数 - 访问远程 - 订单信息服务器8082 - 如果调不通就走断路器");
+        return restTemplate.getForObject("http://127.0.0.1:8082/getOrderTimeOut", String.class);
+    }
+
+    /**
+     * 请求超时触发降级 - 回退方案(降级处理方案)
+     *
+     * @param num 请求参数
+     * @return 结果
+     */
+    public String zswTimeoutFallback(Integer num) {
+        // 可以在此做兜底的数据处理,例如从广告服务器上查询最新的广告失败了,可以返回固定的广告信息
+        System.out.println("TestHystrixController zswTimeoutFallback ------ num: " + num + "  时间: "
+                + simpleDateFormat.format(new Date()));
+        return "请求超时触发降级 - zswTimeoutFallback - 回退方案 - 请求失败!!!";
     }
 
 }
