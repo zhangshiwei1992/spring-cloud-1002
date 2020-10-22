37e26119695419e13131c34fed1c5f449bf93aaa
zhangshiwei <zhangshiwei199204@163.com>
2020-10-22 17:00:22 +0800

    构建商品goods模块

---
+++ spring-cloud-goods-8083/goods-service-provider/src/main/java/com/zsw/goodsserviceprovider/GoodsServiceProviderApplication.java
@@ -0,0 +1,13 @@
+package com.zsw.goodsserviceprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodsServiceProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoodsServiceProviderApplication.class, args);
+    }
+
+}
---
+++ spring-cloud-goods-8083/goods-service-provider/src/test/java/com/zsw/goodsserviceprovider/GoodsServiceProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zsw.goodsserviceprovider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodsServiceProviderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
